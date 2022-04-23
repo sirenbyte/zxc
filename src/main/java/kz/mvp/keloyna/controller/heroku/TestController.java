2d7870b0aff0a9aa30a0d090d7aa1cc56e96eb40
Abzal Tuganbay <abzal.tugan@gmail.com>
2022-04-23 16:45:21 +0600

    make

---
+++ src/main/java/kz/mvp/keloyna/controller/heroku/TestController.java
@@ -0,0 +1,21 @@
+package kz.mvp.keloyna.controller.heroku;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/v1/api/heroku")
+@AllArgsConstructor
+@Slf4j
+public class TestController {
+    @GetMapping("/")
+    public void getTime() {
+        log.info("Time request {}", LocalDateTime.now());
+    }
+
+}
