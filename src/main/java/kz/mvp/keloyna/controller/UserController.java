da52b326b294f9ff2332a489472ce1326527c9fa
Abzal Tuganbay <abzal.tugan@gmail.com>
2022-04-30 19:02:24 +0600

    asd

--- src/main/java/kz/mvp/keloyna/controller/UserController.java
+++ src/main/java/kz/mvp/keloyna/controller/UserController.java
@@ -2,14 +2,33 @@ package kz.mvp.keloyna.controller;
 
 import kz.mvp.keloyna.service.api.UserService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("v1/api/user")
 @AllArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/time")
+    public void getTime() {
+        log.info("Time request {}", LocalDateTime.now());
+    }
+
+    @Scheduled(fixedRate = 1200000)
+    public void loged() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://keloyna.herokuapp.com/time";
+        restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+    }
 }
